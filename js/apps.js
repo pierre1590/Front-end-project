38c9396afea8a0ea3b2fd36652fb01a27588d725
Piero Sabino <=piero.sa@icloud.comgit config --global user.email piero.sa@icloud.comgit config --global user.email =piero.sa@icloud.com>
2021-03-25 21:38:42 +0100

    Update frontend project

--- js/apps.js
+++ js/apps.js
@@ -1,38 +1,93 @@
 const research = document.getElementById('search');
 const notify = document.getElementById('notifications');
 const btnSearch = document.getElementById('searchBtn');
-const result = document.querySelector('.result');
+const result = document.getElementById('results');
+const form = document.getElementById('form');
 
-const apiURL = 'https://api.lyrics.ovh/v1';
+// API URL
+const apiURL = 'https://api.lyrics.ovh';
 
-// SEARCH BY SONG OR ARTIST 
-async function searchSongs(term){
-  const res = await fetch(`${apiURL}/suggest/${term}`);
-  const data = await res.json();
-  showData(data);
-}
+// ADD EVENT LISTENER
 
-// SHOW DATA
-function showData(data){
-  let output = '';
-
-  data.data.forEach(song => {
-    output += `
-    <li>
-    <span><strong>${song.artist.name}</strong> - ${song.title}</span>
-    <button class="btn">Add</btn>`
-  })
-}
-
-// EVENT LISTENERS
-btnSearch.addEventListener('submit', e => {
+form.addEventListener('submit', e => {
   e.preventDefault();
+  searchValue = research.value.trim()
 
-  const searchTerm = research.value.trim();
-
-  if (!searchTerm){
-    alert('Please type in a search term');
-  } else {
-    searchSongs(searchTerm);
+  if (!searchValue){
+    alert("There is nothing to search")
   }
-});
+  else {
+    searchSong(searchValue);
+  }
+
+  
+})
+
+// PRESS ENTER TO SEARCH
+research.addEventListener('keydown', function(e){
+  if(e.keyCode === 13){
+    searchSong();
+  }
+})
+
+// SEARCH SONG
+
+async function searchSong(searchValue){
+  const searchResult = await fetch(`${apiURL}/suggest/${searchValue}`);
+  const data = await searchResult.json();
+ 
+  showData(data);
+
+}
+
+// DISPLAY FINAL RESULT
+function showData(data){
+  notify.innerHTML = ("Searched for:  " +research.value);
+  
+  result.innerHTML = `
+  <ul class="song-list">
+    ${data.data
+      .map(song=> `<li>
+                  <div>
+                      <strong>${song.artist.name}</strong> -${song.title} 
+                  </div>
+                  <span data-artist="${song.artist.name}" data-songtitle="${song.title}">Get lyrics</span>
+              </li>`
+      )
+      .join('')}
+  </ul>
+`;
+
+
+  
+
+
+}
+
+//EVENT LISTENER IN GET LYRICS BUTTON
+result.addEventListener('click', e=>{
+  const clickedElement = e.target;
+
+  //CHECKING CLICKED ELEMENT IS BUTTON OR NOT
+  if (clickedElement.tagName === 'SPAN'){
+      const artist = clickedElement.getAttribute('data-artist');
+      const songTitle = clickedElement.getAttribute('data-songtitle');
+      
+      getLyrics(artist, songTitle)
+  }
+})
+
+// GET LYRICS FOR SONG
+async function getLyrics(artist, songTitle) {
+  const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+  const data = await res.json();
+
+  const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+
+  results.innerHTML = `<button><i class="fas fa-arrow-circle-left "></i></button>
+                       <button><i class="fas fa-plus plus"></i></button>
+          <h2><strong>${artist}</strong> - ${songTitle}</h2>
+          <p>${lyrics}</p>`;
+}
+
+
