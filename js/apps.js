dc76abfad3999a321f66bb403e6f2a4b62f9d04a
Piero Sabino <=piero.sa@icloud.comgit config --global user.email piero.sa@icloud.comgit config --global user.email =piero.sa@icloud.com>
2021-03-26 18:39:29 +0100

    Add prev and next button

--- js/apps.js
+++ js/apps.js
@@ -1,9 +1,10 @@
 const research = document.getElementById('search');
-const notify = document.getElementById('notifications');
+const notify = document.getElementById('searched-text');
 const btnSearch = document.getElementById('searchBtn');
 const result = document.getElementById('results');
 const form = document.getElementById('form');
-
+const notes = document.getElementById('notifications');
+const clear = document.getElementById('reset');
 // API URL
 const apiURL = 'https://api.lyrics.ovh';
 
@@ -40,43 +41,59 @@ async function searchSong(searchValue){
 
 }
 
+
+
 // DISPLAY FINAL RESULT
+
 function showData(data){
+ 
   notify.innerHTML = ("Searched for:  " +research.value);
   
   result.innerHTML = `
   <ul class="song-list">
     ${data.data
       .map(song=> `<li>
-                  <div>
+                    <span>
                       <strong>${song.artist.name}</strong> -${song.title} 
-                  </div>
-                  <span data-artist="${song.artist.name}" data-songtitle="${song.title}">Get lyrics</span>
+                    </span>
+                  <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
               </li>`
       )
       .join('')}
   </ul>
 `;
 
-
-  
-
-
+  if (data.prev || data.next) {
+    more.innerHTML = `
+    ${data.prev ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>` : ''}
+    ${data.next ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>` : ''}
+    `;
+  } else {
+    more.innerHTML = '';
+  }
 }
 
-//EVENT LISTENER IN GET LYRICS BUTTON
+//MORE SONGS
+async function getMoreSongs(url){
+  const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+  const data = await res.json();
+  result.innerHTML = '';
+  showData(data);
+}
+
+//event listener in get lyrics button
 result.addEventListener('click', e=>{
-  const clickedElement = e.target;
+    const clickedElement = e.target;
 
-  //CHECKING CLICKED ELEMENT IS BUTTON OR NOT
-  if (clickedElement.tagName === 'SPAN'){
-      const artist = clickedElement.getAttribute('data-artist');
-      const songTitle = clickedElement.getAttribute('data-songtitle');
-      
-      getLyrics(artist, songTitle)
-  }
+    //checking clicked elemet is button or not
+    if (clickedElement.tagName === 'BUTTON'){
+        const artist = clickedElement.getAttribute('data-artist');
+        const songTitle = clickedElement.getAttribute('data-songtitle');
+        
+        getLyrics(artist, songTitle)
+    }
 })
-
+  
 // GET LYRICS FOR SONG
 async function getLyrics(artist, songTitle) {
   const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
@@ -84,10 +101,14 @@ async function getLyrics(artist, songTitle) {
 
   const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
 
-  results.innerHTML = `<button><i class="fas fa-arrow-circle-left "></i></button>
-                       <button><i class="fas fa-plus plus"></i></button>
+  results.innerHTML = `<button class="back"><i class="fas fa-arrow-circle-left "></i></button>
+                       <button onclick="addToList()" class="add"><i class="fas fa-plus"></i></button>
           <h2><strong>${artist}</strong> - ${songTitle}</h2>
           <p>${lyrics}</p>`;
 }
 
+
+ function addToList(){
+  notes.innerHTML = ("Song successfully added to favorite list");
+}
 
